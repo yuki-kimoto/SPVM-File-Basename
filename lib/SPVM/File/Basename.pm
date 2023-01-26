a0614651e4baa6ab647c5f579d362ab85e40710b
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-26 13:37:17 +0900

    Initial commit

---
+++ lib/SPVM/File/Basename.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Basename;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::File::Basename - Short Description
+
+=head1 Description
+
+C<SPVM::File::Basename> is the C<File::Basename> class in L<SPVM> language.
+
+=head1 Usage
+
+  use File::Basename;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
