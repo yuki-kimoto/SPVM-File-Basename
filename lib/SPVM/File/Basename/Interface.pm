d43063d5432ef088fb53b0e5e8a9f408eafca333
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-26 16:39:11 +0900

    Improve doc

--- lib/SPVM/File/Basename/Interface.pm
+++ lib/SPVM/File/Basename/Interface.pm
@@ -6,40 +6,47 @@ package SPVM::File::Basename::Interface;
 
 =head1 Name
 
-SPVM::File::Basename::Interface - Short Description
+SPVM::File::Basename::Interface - File::Basename Interface
 
 =head1 Description
 
 C<SPVM::File::Basename::Interface> is the C<File::Basename::Interface> class in L<SPVM> language.
 
+This class is the L<File::Basename|SPVM::File::Basename> interface.
+
 =head1 Usage
 
   use File::Basename::Interface;
-  
-=head1 Fields
 
+=head1 Interface Methods
 
+=head2 fileparse
 
-=head1 Class Methods
+  method fileparse : string[] ($path : string);
 
+=head2 basename
 
+  method basename : string ($path : string);
 
-=head1 Instance Methods
+=head2 dirname
 
+  method dirname : string ($path : string);
 
+=head2 has_interfaces
 
-=head1 Repository
+  required method has_interfaces : int ();
 
+=head1 Well Known Implementation Classes
 
+=over 2
 
-=head1 Author
+=item * L<File::Basename|SPVM::File::Basename>
 
-[--user-name] C<[--user-email]>
+=back
 
 =head1 Copyright & License
 
-Copyright 2023-2023 [--user-name], all rights reserved.
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
--- lib/SPVM/File/Basename/Win32.pm
+++ lib/SPVM/File/Basename/Win32.pm
@@ -6,40 +6,43 @@ package SPVM::File::Basename::Win32;
 
 =head1 Name
 
-SPVM::File::Basename::Win32 - Short Description
+SPVM::File::Basename::Win32 - File::Basename Implementation for Windows.
 
 =head1 Description
 
 C<SPVM::File::Basename::Win32> is the C<File::Basename::Win32> class in L<SPVM> language.
 
+This class is a child class of the L<File::Basename|SPVM::File::Basename> class for Windows.
+
 =head1 Usage
 
   use File::Basename::Win32;
   
-=head1 Fields
+  my $fb = File::Basename::Win32->new;
 
+=head1 Inheritance
 
+=over 2
+
+=item * L<File::Basename::Unix|SPVM::File::Basename::Unix>
+
+=back
 
 =head1 Class Methods
 
+=head2 new
 
+  static method new : File::Basename::Win32 ();
 
 =head1 Instance Methods
 
+=head2 fileparse
 
-
-=head1 Repository
-
-
-
-=head1 Author
-
-[--user-name] C<[--user-email]>
+  method fileparse : string[] ($path : string);
 
 =head1 Copyright & License
 
-Copyright 2023-2023 [--user-name], all rights reserved.
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
